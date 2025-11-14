28585b7b9709251343fbe89f421e1548e1df48e6
0xhaz <hazrienne@gmail.com>
2025-11-14 21:50:57 +0800

    Liquidity exercise

---
+++ test/UniswapV2Liquidity.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import {Test, console} from "forge-std/Test.sol";
+import {IERC20} from "../src/interfaces/IERC20.sol";
+import {IWETH} from "../src/interfaces/IWETH.sol";
+import {IUniswapV2Router02} from "../src/interfaces/IUniswapV2Router02.sol";
+import {IUniswapV2Pair} from "../src/interfaces/IUniswapV2Pair.sol";
+import {DAI, WETH, UNISWAP_V2_ROUTER_02, UNISWAP_V2_PAIR_DAI_WETH} from "../src/Constants.sol";
+
+contract UniswapV2LiquidityTest is Test {
+    IWETH private constant weth = IWETH(WETH);
+    IERC20 private constant dai = IERC20(DAI);
+
+    IUniswapV2Router02 private constant router = IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
+    IUniswapV2Pair private constant pair = IUniswapV2Pair(UNISWAP_V2_PAIR_DAI_WETH);
+
+    address private constant user = address(100);
+
+    function setUp() public {
+        deal(user, 100 ether);
+        vm.startPrank(user);
+        weth.deposit{value: 100 ether}();
+        weth.approve(address(router), type(uint256).max);
+        vm.stopPrank();
+
+        deal(DAI, user, 1e6 * 1e18); // 1,000,000 DAI
+        vm.startPrank(user);
+        dai.approve(address(router), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function test_addLiquidity() public {
+        vm.prank(user);
+
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router.addLiquidity({
+            tokenA: DAI, // tokenA
+            tokenB: WETH, // tokenB
+            amountADesired: 1e6 * 1e18, // 1,000,000 DAI (amountADesired)
+            amountBDesired: 100 ether, // 100 WETH (amountBDesired)
+            amountAMin: 1, // amountAMin
+            amountBMin: 1, // amountBMin
+            to: user, // to
+            deadline: block.timestamp + 15
+        });
+
+        console.log("DAI added:", amountA);
+        console.log("WETH added:", amountB);
+        console.log("LP tokens minted:", liquidity);
+
+        assertGt(pair.balanceOf(user), 0);
+    }
+}
